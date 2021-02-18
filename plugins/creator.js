b8e7333531437c0babe0b217bc0d4141507aaa6b
Drawl Nag <75187284+Arya274@users.noreply.github.com>
2021-02-18 11:34:41 +0700

    Add files via upload

---
+++ plugins/creator.js
@@ -0,0 +1,10 @@
+let handler = function (m) {
+  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
+  this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+
+handler.command = /^(owner|creator)$/i
+
+module.exports = handler
