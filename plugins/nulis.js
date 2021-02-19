e555aace6306213adbcc29a23afda61322b1c8ae
Drawl Nag <75187284+Arya274@users.noreply.github.com>
2021-02-20 05:53:19 +0700

    Update nulis

--- plugins/nulis.js
+++ plugins/nulis.js
@@ -51,7 +51,7 @@ let handler  = async (m, { conn, args }) => {
   ])
   .on('error', e => conn.reply(m.chat, util.format(e), m))
   .on('exit', () => {
-    conn.sendFile(m.chat, outputPath, 'nulis.jpg', 'Hati² ketahuan:v', m)
+    conn.sendFile(m.chat, outputPath, 'nulis.jpg', 'Nihh cuk.. tulisanmu. Mager Lu?\n Instagram: @arpunchs', m)
   })
 }
 handler.help = ['n'].map(v => v + 'ulis <teks>')
