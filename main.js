d5b33b48647e8664cd2d9f7deeaae746a835d603
Drawl Nag <75187284+Arya274@users.noreply.github.com>
2021-02-24 19:06:49 +0700

    --restrict

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'Ecd203fgm'
+  'https://api.xteam.xyz': 'test'
 }
 
 
