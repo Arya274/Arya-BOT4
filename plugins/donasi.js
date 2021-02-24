f8f9ae497afc8630c36a2bc3501c7608ea32ce51
rendy2404 <79277263+rendy2404@users.noreply.github.com>
2021-02-24 13:37:53 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
 │ • Smartfren [088235435804]
-│ • Telkomsel [081357302007]
+│ • Telkomsel [081262309422]
 │ • Gopay [081357302007]
 ╰────
 ╭─「 Hubungi 」
@@ -13,7 +13,7 @@ Ini *#caranya untuk Donasi*
 1.) Beli ke pulsa/ konter terdekat semisal Indomaret
 2.) Bilang ke konter terdekat..
 "Beli pulsa telkomsel"
-3.)Dan terus masukkan nomor kami 081357302007
+3.)Dan terus masukkan nomor kami 081262309422
 4.) Jika sudah kirim bukti ke sini.. Terimakasih
 *Kalau tidak juga gak papa*👍🏻
 `.trim()) // Tambah sendiri kalo mau
